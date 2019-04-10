5f6108500d9eb7280a45a6ea74a4f007e7d059ee
Jeewoo Lee <jlee@lcs.on.ca>
2019-04-10 11:29:16 -0400

    Got frame tracking showing in the world

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -9,10 +9,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Piano extends World
 {
     /**
+     * Instance Variables (can be used anywhere below)
+     */
+    int frames;
+    
+    /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
+        
+        // STtart tracking frames
+        frames =0;
+    }
+    /**
+     * act - called repeatedly to create animation
+     */
+    public void act()
+    {
+        //Keep track of the frames
+        frames ++;
+        
+        showText("" + frames,100, 100);
     }
 }
