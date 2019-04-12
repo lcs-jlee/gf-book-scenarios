dd21fdfe342c5f3f642f5c57b2e3c2fb9bec97a1
Jeewoo Lee <jlee@lcs.on.ca>
2019-04-12 15:15:58 -0400

    use parameters in the constructor....using abstraction to make the key class work for many different notes

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -3,16 +3,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     //tracks whether is "piano key down" image has already been set or not.
+    //isDown is primitive data type
     private boolean isDown;
+    //Track what key is being played AND what sound file to play
+    //key and sound are object data types -shows in black, type in Capitalized
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      * Constructor
      */
-    public Key()
+    public Key(String keyName,String soundFile)
     {
         //Key begins in the "up" position
         isDown = false;
         
+        //Initialize the key and sound instance variables
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -23,13 +31,13 @@ public class Key extends Actor
         //when the key image is not already showing "down"
         //AND
         //the "g"key on the keyboard has been pressed
-        if (!isDown && Greenfoot.isKeyDown("g"))
+        if (!isDown && Greenfoot.isKeyDown(key))
         {
             setImage("white-key-down.png");
             play();
             isDown = true;
         }
-        if (isDown == true && !Greenfoot.isKeyDown("g"))
+        if (isDown == true && !Greenfoot.isKeyDown(key))
         {
             setImage("white-key.png");
             isDown = false;
@@ -40,7 +48,7 @@ public class Key extends Actor
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav");
+        Greenfoot.playSound(sound + ".wav");
         
     }
 }
