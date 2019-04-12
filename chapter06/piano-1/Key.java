61bcd46694d798977ce49812a433bbcd5fc43165
Jeewoo Lee <jlee@lcs.on.ca>
2019-04-12 14:43:58 -0400

    Made the key image switching happen more efficiently

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -2,12 +2,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    //tracks whether is "piano key down" image has already been set or not.
+    private boolean isDown;
     /**
      * Create a new key.
      * Constructor
      */
     public Key()
     {
+        //Key begins in the "up" position
+        isDown = false;
         
     }
 
@@ -16,14 +20,18 @@ public class Key extends Actor
      */
     public void act()
     {
-        if (Greenfoot.isKeyDown("g"))
+        //when the key image is not already showing "down"
+        //AND
+        //the "g"key on the keyboard has been pressed
+        if (!isDown && Greenfoot.isKeyDown("g"))
         {
             setImage("white-key-down.png");
+            isDown = true;
         }
-        else
+        if (isDown == true && !Greenfoot.isKeyDown("g"))
         {
             setImage("white-key.png");
-        
+            isDown = false;
         }
     }
 }
